9e2497fa61e4efd694c82a246fcbc47daeca50eb
Shrikant Kale <kaleshrikant@live.com>
2024-08-28 22:43:20 +0530

    dto created, base-domain

---
+++ base-domains/src/main/java/net/javaguides/basedomains/dto/Order.java
@@ -0,0 +1,19 @@
+package net.javaguides.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+
+    private String orderId;
+    private String name;
+    private int qty;
+    private double price;
+
+}
