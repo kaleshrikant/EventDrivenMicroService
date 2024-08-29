1ecc4c4414f1e472fea79df67ce7f2e42a676315
Shrikant Kale <kaleshrikant@live.com>
2024-08-29 10:46:15 +0530

    added OrderConsumer.java

---
+++ stock-service/src/main/java/net/javaguides/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,23 @@
+package net.javaguides.stockservice.kafka;
+
+import net.javaguides.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Order Event Recived In Stock Service => %s ", String.valueOf(orderEvent)));
+
+        // You can : save the order event into the database.
+    }
+}
